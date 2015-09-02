db6b812904e8f8855898d132b28d6b7b7a91355b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-09-02 15:51:26 +0200

    update windows setup
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ scripts/windows-7.1.0-64/package.ps1
@@ -0,0 +1,62 @@
+Enable-RemoteDesktop
+Set-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP -Enabled True
+
+Write-BoxstarterMessage "Removing page file"
+$pageFileMemoryKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management"
+Set-ItemProperty -Path $pageFileMemoryKey -Name PagingFiles -Value ""
+
+Update-ExecutionPolicy -Policy Unrestricted
+
+Write-BoxstarterMessage "Removing unused features..."
+Remove-WindowsFeature -Name 'Powershell-ISE'
+Get-WindowsFeature | 
+? { $_.InstallState -eq 'Available' } | 
+Uninstall-WindowsFeature -Remove
+
+Install-WindowsUpdate -AcceptEula
+if(Test-PendingReboot){ Invoke-Reboot }
+
+Write-BoxstarterMessage "Cleaning SxS..."
+Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
+
+@(
+    "$env:localappdata\Nuget",
+    "$env:localappdata\temp\*",
+    "$env:windir\logs",
+    "$env:windir\panther",
+    "$env:windir\temp\*",
+    "$env:windir\winsxs\manifestcache"
+) | % {
+        if(Test-Path $_) {
+            Write-BoxstarterMessage "Removing $_"
+            Takeown /d Y /R /f $_
+            Icacls $_ /GRANT:r administrators:F /T /c /q  2>&1 | Out-Null
+            Remove-Item $_ -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+        }
+    }
+
+Write-BoxstarterMessage "defragging..."
+Optimize-Volume -DriveLetter C
+
+Write-BoxstarterMessage "0ing out empty space..."
+wget http://download.sysinternals.com/files/SDelete.zip -OutFile sdelete.zip
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+[System.IO.Compression.ZipFile]::ExtractToDirectory("sdelete.zip", ".") 
+./sdelete.exe /accepteula -z c:
+
+mkdir C:\Windows\Panther\Unattend
+copy-item a:\postunattend.xml C:\Windows\Panther\Unattend\unattend.xml
+
+Write-BoxstarterMessage "Recreate pagefile after sysprep"
+$System = GWMI Win32_ComputerSystem -EnableAllPrivileges
+$System.AutomaticManagedPagefile = $true
+$System.Put()
+
+Write-BoxstarterMessage "Setting up winrm"
+Set-NetFirewallRule -Name WINRM-HTTP-In-TCP-PUBLIC -RemoteAddress Any
+Enable-WSManCredSSP -Force -Role Server
+
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
