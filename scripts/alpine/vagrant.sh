6bbce0cbb9c11fe7d326499bee68b41b1c08c244
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-07-03 14:24:39 +0200

    Customization of AlpineLinux boxes
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- scripts/alpine/vagrant.sh
+++ scripts/alpine/vagrant.sh
@@ -2,12 +2,11 @@ set -exu
 
 apk add bash curl
 
-adduser -D vagrant
-echo "vagrant:vagrant" | chpasswd
+(echo $VAGRANT_PASSWORD; echo $VAGRANT_PASSWORD) | adduser -s /bin/ash $VAGRANT_USERNAME
 
-mkdir -pm 700 /home/vagrant/.ssh
+mkdir -pm 700 /home/$VAGRANT_USERNAME/.ssh
 
-curl -sSo /home/vagrant/.ssh/authorized_keys 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+curl -sSo /home/$VAGRANT_USERNAME/.ssh/authorized_keys 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
 
-chown -R vagrant:vagrant /home/vagrant/.ssh
-chmod -R go-rwsx /home/vagrant/.ssh
+chown -R $VAGRANT_USERNAME:$VAGRANT_USERNAME /home/$VAGRANT_USERNAME
+chmod -R og-rwx /home/$VAGRANT_USERNAME/.ssh
